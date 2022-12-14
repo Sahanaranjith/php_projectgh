e6f370b94878379510fb6f6ed9c16a3c6417f603
sahana170498 <sahanammr4@gmail.com>
2022-12-14 11:00:54 +0530

    added examples in new file

---
+++ example1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <style>
+        h1 {
+            color: green;
+        }
+    </style>
+</head>
+  
+<body>
+    <center>
+        <h1>Welcome To GFG</h1>
+  
+        <h2>
+            <?php 
+            echo "This is PHP code inside html";
+            echo "added a line";
+            ?>
+        </h2>
+    </center>
+</body>
+  
+</html>
